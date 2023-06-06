9a8dfc9cbabdd7a45b061c51fd80d7513a982fd9
Yujie Hu <78907155+breeze-maple@users.noreply.github.com>
2023-06-06 17:16:40 +0800

    feat

--- src/main/java/com/ybuse/schoolbackend/pmi_type/controller/PmiTypeController.java
+++ src/main/java/com/ybuse/schoolbackend/pmi_type/controller/PmiTypeController.java
@@ -1,11 +1,11 @@
-package com.ybuse.schoolbackend.class_name.controller;
+package com.ybuse.schoolbackend.pmi_type.controller;
 
-import com.ybuse.schoolbackend.class_name.domain.po.ClassNamePo;
-import com.ybuse.schoolbackend.class_name.service.IClassNameService;
 import com.ybuse.schoolbackend.core.ApiV1Controller;
 import com.ybuse.schoolbackend.core.domain.vo.CommonResult;
 import com.ybuse.schoolbackend.core.logger.MethodType;
 import com.ybuse.schoolbackend.core.logger.annotation.PrintLog;
+import com.ybuse.schoolbackend.pmi_type.domain.po.PmiTypePo;
+import com.ybuse.schoolbackend.pmi_type.service.IPmiTypeService;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
-
 
 /**
  @Author: hyj
@@ -27,50 +26,49 @@ import java.util.Map;
 @PrintLog(
         methodType = MethodType.HTTP_UP
 )
-@Tag(name = "ActiveManagerController", description = "ActiveManagerController", externalDocs = @ExternalDocumentation(description = "Swagger3(OpenAPI)常用注解参考", url = "https://blog.csdn.net/qq_35425070/article/details/105347336"))
-@ApiV1Controller("/class")
-public class ClassNameController {
-    /**
-     * 服务对象
-     */
-    private final IClassNameService classNameService;
+@Tag(name = "PmiTypeController", description = "用于操作class的api", externalDocs = @ExternalDocumentation(description = "Swagger3(OpenAPI)常用注解参考", url = "https://blog.csdn.net/qq_35425070/article/details/105347336"))
+@ApiV1Controller("/pmiType")
+public class PmiTypeController {
 
-    public ClassNameController(IClassNameService classNameService) {
-        this.classNameService = classNameService;
+    private final IPmiTypeService pmiTypeService;
+
+    public PmiTypeController(IPmiTypeService pmiTypeService) {
+        this.pmiTypeService = pmiTypeService;
     }
 
     @PostMapping("/post")
     @Operation(summary = "create class")
-    public CommonResult<Object> addClass(@RequestBody ClassNamePo classNamePo) {
-        classNameService.add(classNamePo);
+    public CommonResult<Object> addClass(@RequestBody PmiTypePo pmiTypePo) {
+        pmiTypeService.add(pmiTypePo);
         return CommonResult.success("ok");
     }
 
     @DeleteMapping("/delete/{id}")
     @Operation(summary = "delete active")
     public CommonResult<Object> deleteClass(@PathVariable("id") int id) {
-        classNameService.removeById(id);
+        pmiTypeService.removeById(id);
         return CommonResult.success("ok");
     }
 
     @PutMapping("/put")
     @Operation(summary = "put active")
-    public CommonResult<Object> putClass(@RequestBody ClassNamePo classNamePo) {
-        classNameService.updateById(classNamePo);
+    public CommonResult<Object> putClass(@RequestBody PmiTypePo pmiTypePo) {
+        pmiTypeService.updateById(pmiTypePo);
         return CommonResult.success("ok");
     }
 
     @GetMapping("/get")
     @Operation(summary = "get active")
     public CommonResult<Object> getAllClass() {
-        val maps = classNameService.findAll().stream().map(classNamePo -> {
+        val maps = pmiTypeService.findAll().stream().map(classNamePo -> {
             Map<String, Object> map = new HashMap<>(16);
             map.put("id", classNamePo.getId());
-            map.put("name", classNamePo.getClassName());
+            map.put("typeName", classNamePo.getTypeName());
             return map;
         }).toList();
         System.out.println(maps);
         return CommonResult.success(maps);
     }
-}
 
+
+}
---
+++ src/main/java/com/ybuse/schoolbackend/pmi_type/controller/package-info.java
@@ -0,0 +1,4 @@
+@SwaggerMark("授奖种类模块管理相关")
+package com.ybuse.schoolbackend.pmi_type.controller;
+
+import com.ybuse.schoolbackend.core.swagger.annotation.SwaggerMark;
